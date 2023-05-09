053e6b386ea0f76403f13cad9b11018cedac333a
Alexis Aostri <aaostri@mobilecomputing.com.ar>
2023-05-09 00:15:28 -0300

    .

--- src/controllers/animalsMongoDB.controller.js
+++ src/controllers/animalsMongoDB.controller.js
@@ -11,23 +11,12 @@ export const getAllAnimals = async (req, res) => {
 
 export const createAnimal = async (req, res) => {
     console.log("Req body", req.body)
-
     try {
-
         const { name, typeAnimal, age } = req.body;
         const animal = new Animal({ name, typeAnimal, age });
         const animalSave = await animal.save();
         return res.json(animalSave);
     } catch (error) {
-        console.log("error animal: ", error)
         res.json({"errorMessage":error})
     }
-
-    // try {
-    //     console.log("req",req.uid)
-    //     const user = await User.findById(req.uid); //Este req.uid es la propiedad q le setie al requerimiento en el middleware de requireToken, el id lo saque de la variable payload.
-    //     return res.json({email:user.email, id: user.id}); //No quiero que le envie el password al cliente
-    // } catch (error) {
-    //    return res.status(500).json({error: "Error en el servidor"}) 
-    // }
 }
