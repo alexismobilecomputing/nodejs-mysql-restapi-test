285a297433579d9b2ce5bd1cb61414c47b9b13f1
Alexis Aostri <aaostri@mobilecomputing.com.ar>
2023-05-11 18:15:33 -0300

    .

--- src/controllers/animalsMongoDB.controller.js
+++ src/controllers/animalsMongoDB.controller.js
@@ -2,7 +2,8 @@ import { Animal } from '../models/Animal.js'
 
 export const getAllAnimals = async (req, res) => {
     try {
-        const animals = await Animal.find({}, {_id: 0,__v:0}); //Con esto le digo traeme todos los campos menos el _id y el __v
+        // const animals = await Animal.find({}, {_id: 0,__v:0}); //Con esto le digo traeme todos los campos menos el _id y el __v
+        const animals = await Animal.find({}, {__v:0}); //Con esto le digo traeme todos los campos menos el __v
         return res.json(animals); //Me devuelve todos los animales
     } catch (error) {
        return res.status(500).json({error: "Error en el servidor"}) 
@@ -15,10 +16,27 @@ export const createAnimal = async (req, res) => {
         const { name, typeAnimal, age } = req.body;
         const animal = new Animal({ name, typeAnimal, age });
         const animalSave = await animal.save();
+        // console.log("ANIMAL SAVE: ",animalSave)
         return res.json(animalSave);
     } catch (error) {
         if(error.code === 11000){
             res.status(400).json({"errorMessage":`El nombre ${error.keyValue.name} ya existe`})
         }
+    }
+}
+
+export const deleteAnimal = async (req, res) => {
+    console.log("Req body", req.body)
+    try {
+        // const { id } = req.body; //El id q me envia el cliente, 
+        // const animal = new Animal({ name, typeAnimal, age });
+        // const animalSave = await animal.save();
+        // console.log("ANIMAL SAVE: ",animalSave)
+        // return res.json(animalSave);
+    } catch (error) {
+        console.log("error")
+        // if(error.code === 11000){
+        //     res.status(400).json({"errorMessage":`El nombre ${error.keyValue.name} ya existe`})
+        // }
     }
 }
