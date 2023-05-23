a73faa920cc81a3edb21753a5a12de9ce55dc21d
Alexis Aostri <aaostri@mobilecomputing.com.ar>
2023-05-23 16:14:11 -0300

    ..

--- src/controllers/usuarioLoginMongoDB.controller.js
+++ src/controllers/usuarioLoginMongoDB.controller.js
@@ -125,9 +125,6 @@ export const login = async (req, res) => {
         //     secure: !(process.env.MODO === "developer") //Esto es para que viva en https, pero nosotros cuando trabajamos local usamos http, entonces le prgeuntamos a la variable de entorno modo q creamos para q si estamos en local ponga en false sino en true, en produccion siempre tiene q estar en true
         // }) 
 
-        console.log("GENERATE LOGIN:")
-        console.log("GENERATE REFRESH TOKEN:")
-
         generateRefreshToken(usuario.id,res)
 
         return res.json({ token: token, expiresIn }) //Se puede escribir de las 2 maneras si los nombres coinciden
